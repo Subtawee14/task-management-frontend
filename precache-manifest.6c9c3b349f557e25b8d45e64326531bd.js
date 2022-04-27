2854513f6f6aae2ccf954267e96cbfce516efe2c
Subtawee14 <Subtawee14@users.noreply.github.com>
2022-04-27 05:48:08 +0000

    deploy: 236fbcd78465609cfcc5c2d2482f7adebe6a3057

---
+++ precache-manifest.6c9c3b349f557e25b8d45e64326531bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d75e313a85cbfa785f2798111b3be5a",
+    "url": "https://Subtawee14.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "83ca38de3f3ee9c7aae6",
+    "url": "https://Subtawee14.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "ebaa072f1776ed3f762f",
+    "url": "https://Subtawee14.github.io/task-management-frontend/static/js/2.15c8fa49.chunk.js"
+  },
+  {
+    "revision": "83ca38de3f3ee9c7aae6",
+    "url": "https://Subtawee14.github.io/task-management-frontend/static/js/main.e87b687d.chunk.js"
+  },
+  {
+    "revision": "6f8970c85b8cb58a93a9",
+    "url": "https://Subtawee14.github.io/task-management-frontend/static/js/runtime~main.53922a27.js"
+  }
+]);
